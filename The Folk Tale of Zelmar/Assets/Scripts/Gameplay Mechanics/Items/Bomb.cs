2a2a6a07e6c16b0e99af6257780b3c327573acfb
DangubaGitHub <dangubicjovan@outlook.com>
2022-04-30 17:44:04 +0200

    commit 007
    added bomb and cracked wall animations

---
+++ The Folk Tale of Zelmar/Assets/Scripts/Gameplay Mechanics/Items/Bomb.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    Animator bombAnim;
+    string currentState;
+
+    const string BOMB = "Bomb";
+
+    private void Awake()
+    {
+        bombAnim = GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+        
+        ChangeAnimationState(BOMB);
+        
+    }
+
+
+    void Update()
+    {
+        Destroy(gameObject, 2.2f);
+    }
+
+    void ChangeAnimationState(string newState)
+    {
+        if (currentState == newState) return;
+
+        bombAnim.Play(newState);
+
+        currentState = newState;
+    }
+}
