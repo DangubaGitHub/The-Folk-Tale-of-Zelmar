5a53dc6e8e3ef753794defbb247ff102bf746e12
DangubaGitHub <dangubicjovan@outlook.com>
2022-05-25 09:34:54 +0200

    added overworld map points

---
+++ The Folk Tale of Zelmar/Assets/Scripts/Overworld/Overworld_Map_Points.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Overworld_Map_Points : MonoBehaviour
+{
+
+    [SerializeField] Overworld_Map_Points up;
+    [SerializeField] Overworld_Map_Points right;
+    [SerializeField] Overworld_Map_Points down;
+    [SerializeField] Overworld_Map_Points left;
+
+    [SerializeField] bool isLevel;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
